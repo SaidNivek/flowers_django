bcd241e09c1919f391e5326f332c039fd0b00781
SaidNivek <kevin.vi.dias@gmail.com>
2022-06-01 10:45:36 -0400

    Flower list working, Flower model and db working

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 # this like app.use() in express
 urlpatterns = [
     path('', views.Home.as_view(), name="home"),
-    path('about/', views.About.as_view(), name="about")
+    path('about/', views.About.as_view(), name="about"),
+    path('flowers/', views.FlowerList.as_view(), name="flower_list"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View # <- View class to handle requests
 from django.http import HttpResponse # <- a class to handle sending a type of response
 from django.views.generic.base import TemplateView
+from .models import Flower
 
 # Create your views here.
 
@@ -11,3 +12,11 @@ class Home(TemplateView):
 
 class About(TemplateView):
     template_name = "about.html"
+
+class FlowerList(TemplateView):
+    template_name = "flower_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["flowers"] = Flower.objects.all() # this is where we add the key into our context object for the view to use
+        return context
